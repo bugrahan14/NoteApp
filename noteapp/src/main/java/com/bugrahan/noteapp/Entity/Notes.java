acc18656766f529768ca74d1d255294de237991c
bugrahan14 <ertasbugra0611@gmail.com>
2026-02-26 17:03:03 +0300

    first commit

---
+++ noteapp/src/main/java/com/bugrahan/noteapp/Entity/Notes.java
@@ -0,0 +1,5 @@
+package com.bugrahan.noteapp.Entity;
+
+public class Notes {
+
+}
---
+++ noteapp/src/main/java/com/bugrahan/noteapp/Entity/Users.java
@@ -0,0 +1,17 @@
+package com.bugrahan.noteapp.Entity;
+
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id ;
+
+    
+
+
+
+
+
+
+}
